295952d90a88b369a3c8f7967a41aba900ffd21e
Mauricio Godoy <95603787+Mauri-Godoy@users.noreply.github.com>
2024-03-31 20:36:39 -0300

    Resolución tp3

---
+++ src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.mavenproject1;
+
+import com.mycompany.mavenproject1.frames.LoginForm;
+
+
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+        new LoginForm();
+
+    }
+}
---
+++ src/main/java/com/mycompany/mavenproject1/frames/LoginForm.java
@@ -0,0 +1,49 @@
+package com.mycompany.mavenproject1.frames;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class LoginForm extends JFrame implements ActionListener {
+    private JTextField emailField;
+    private JTextField passwordField;
+
+    public LoginForm() {
+        setTitle("Formulario de Login");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(300, 150);
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(3, 2));
+
+        JLabel emailLabel = new JLabel("Email:");
+        emailField = new JTextField();
+        JLabel passwordLabel = new JLabel("Password:");
+        passwordField = new JTextField();
+        JButton registerButton = new JButton("Registrar");
+        registerButton.addActionListener(this);
+
+        panel.add(emailLabel);
+        panel.add(emailField);
+        panel.add(passwordLabel);
+        panel.add(passwordField);
+        panel.add(new JLabel()); // Espacio en blanco
+        panel.add(registerButton);
+
+        add(panel);
+        setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String email = emailField.getText();
+        String password = passwordField.getText();
+
+        if (email.equals("alumno@ulp.edu.ar") && password.equals("12345678")) {
+            JOptionPane.showMessageDialog(this, "¡Bienvenido!");
+        } else {
+            JOptionPane.showMessageDialog(this, "Usuario y/o contraseña incorrectos");
+        }
+    }
+}
